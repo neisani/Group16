6592fda390cc5ad12d58d668a46a346514048d83
neisani <32688406+neisani@users.noreply.github.com>
2019-07-05 00:25:22 -0700

    Add files via upload

---
+++ AuthApi/app/config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'secret': 'wdr-super-secret-key',
+  ROLEs: ['OC', 
+  'OBSERVER', 
+  'MANAGER DATA',
+  'ZONAL OFFICER',
+  'DATA OFFICER',
+  'SENIOR DATA OFFICER',
+  'WEATHER FORECASTER',
+  'MANAGER STATION NETWORKS',
+  'SENIOR ZONAL OFFICER',
+  'WEATHER ANALYST'
+
+]
+};
